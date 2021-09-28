1a1fa9532cf8493bac91756f371e3d8dc9e99ddc
alfieqashwa <alfieqashwa@gmail.com>
2021-09-28 23:34:45 +0700

    [DONE] setup footer desktop view

--- components/Layout.tsx/Footer/Footer.tsx
+++ components/Layout.tsx/Footer/Footer.tsx
@@ -6,6 +6,8 @@ import {
 } from 'react-icons/ai'
 import { SiReverbnation } from 'react-icons/si'
 
+import { DesktopLogo } from 'components/Layout.tsx/Nav/Logo'
+
 type Props = {
   menus: string[]
 }
@@ -14,63 +16,130 @@ function Footer(props: Props): JSX.Element {
   const router = useRouter()
 
   return (
-    <footer className='w-full h-full bg-[#171B20] md:hidden'>
-      <div className='w-11/12 pt-16 mx-auto border-t border-gray-600' />
-      <ul className='pt-4 pb-16 space-y-12'>
-        {props.menus.map((menu) => (
-          <li className='text-center capitalize' key={menu}>
-            <h5 className='font-light tracking-wider'>
+    <>
+      {/* // * Mobile View */}
+      <footer className='w-full h-full bg-[#171B20] md:hidden'>
+        <div className='w-11/12 pt-16 mx-auto border-t border-gray-600' />
+        <ul className='pt-4 pb-16 space-y-12'>
+          {props.menus.map((menu) => (
+            <li className='text-center capitalize' key={menu}>
+              <h5 className='font-light tracking-wider'>
+                <a
+                  onClick={(
+                    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => {
+                    e.preventDefault()
+                    router.push(`/${menu}`)
+                  }}
+                >
+                  {menu}
+                </a>
+                {router.asPath === `/${menu}` && (
+                  <div className='w-5 mx-auto mt-0.5 border-t' />
+                )}
+              </h5>
+            </li>
+          ))}
+        </ul>
+        <section className='flex items-center justify-center space-x-14'>
+          <a
+            href='https://www.twitter.com/alfieqashwa'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiOutlineTwitter className='w-4 h-4 text-trueGray-500' />
+          </a>
+          <a
+            href='https://www.github.com/alfieqashwa'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiOutlineGithub className='w-4 h-4 text-trueGray-500' />
+          </a>
+          <a
+            href='https://www.instagram.com/alfieqashwa'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiOutlineInstagram className='w-4 h-4 text-trueGray-500' />
+          </a>
+          <a
+            href='https://www.reverbnation.com/tweleph'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <SiReverbnation className='w-4 h-4 text-trueGray-500' />
+          </a>
+        </section>
+        <p className='pt-16 pb-20 text-sm tracking-widest text-center text-trueGray-500'>
+          © {new Date().getFullYear()} Alfie Qashwa
+        </p>
+      </footer>
+
+      {/* // * Desktop View */}
+      <footer className='hidden bg-[#171B20] pt-20 pb-16 -mx-20 md:block'>
+        <section className='z-10 flex items-center justify-between px-36'>
+          <nav className='flex items-center justify-between space-x-14'>
+            <DesktopLogo />
+            <section className='flex items-center justify-between space-x-10'>
               <a
-                onClick={(
-                  e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-                ) => {
-                  e.preventDefault()
-                  router.push(`/${menu}`)
-                }}
+                href='https://www.twitter.com/alfieqashwa'
+                target='_blank'
+                rel='noopener noreferrer'
               >
-                {menu}
+                <AiOutlineTwitter className='w-4 h-4 text-trueGray-500' />
               </a>
-              {router.asPath === `/${menu}` && (
-                <div className='w-5 mx-auto mt-0.5 border-t' />
-              )}
-            </h5>
-          </li>
-        ))}
-      </ul>
-      <section className='flex items-center justify-center space-x-14'>
-        <a
-          href='https://www.twitter.com/alfieqashwa'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <AiOutlineTwitter className='w-4 h-4 text-trueGray-500' />
-        </a>
-        <a
-          href='https://www.github.com/alfieqashwa'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <AiOutlineGithub className='w-4 h-4 text-trueGray-500' />
-        </a>
-        <a
-          href='https://www.instagram.com/alfieqashwa'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <AiOutlineInstagram className='w-4 h-4 text-trueGray-500' />
-        </a>
-        <a
-          href='https://www.reverbnation.com/tweleph'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <SiReverbnation className='w-4 h-4 text-trueGray-500' />
-        </a>
-      </section>
-      <p className='pt-16 pb-20 text-sm tracking-widest text-center text-trueGray-500'>
-        © {new Date().getFullYear()} Alfie Qashwa
-      </p>
-    </footer>
+              <a
+                href='https://www.github.com/alfieqashwa'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <AiOutlineGithub className='w-4 h-4 text-trueGray-500' />
+              </a>
+              <a
+                href='https://www.instagram.com/alfieqashwa'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <AiOutlineInstagram className='w-4 h-4 text-trueGray-500' />
+              </a>
+              <a
+                href='https://www.reverbnation.com/tweleph'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <SiReverbnation className='w-4 h-4 text-trueGray-500' />
+              </a>
+            </section>
+          </nav>
+          <ul className='flex items-center space-x-12'>
+            {props.menus.map((menu) => (
+              <li key={menu}>
+                <h6 className='tracking-wider capitalize transition duration-300 ease-in-out hover:opacity-80'>
+                  <a
+                    href={`/${menu}`}
+                    onClick={(
+                      e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                    ) => {
+                      e.preventDefault()
+                      router.push(`/${menu}`)
+                    }}
+                  >
+                    {menu}
+                  </a>
+                  {router.asPath === `/${menu}` && (
+                    <div className='w-5 mx-auto mt-0.5 border-t' />
+                  )}
+                </h6>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <p className='pt-24 text-sm tracking-widest text-center text-trueGray-500'>
+          © {new Date().getFullYear()} Alfie Qashwa
+        </p>
+      </footer>
+    </>
   )
 }
 
