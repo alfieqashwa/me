cfad3118a63e268a8944a45064c345e7df2863c7
alfieqashwa <alfieqashwa@gmail.com>
2021-09-30 00:11:26 +0700

    create glowing card with quote

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -17,10 +17,56 @@ const Hero = () => {
         </h2>
       </section>
       <section className='flex items-center'>
-        <h2 className='sr-only'>Image section</h2>
+        <QuoteCard />
       </section>
     </div>
   )
 }
 
 export default Hero
+
+const QuoteCard = () => {
+  const glowVariants = {
+    hover: {
+      scale: 1.1,
+    },
+    initial: {
+      scale: 1,
+    },
+  }
+
+  return (
+    <motion.div
+      initial='initial'
+      whileHover='hover'
+      className='relative cursor-wait'
+    >
+      <motion.div
+        variants={glowVariants}
+        transition={{ ease: 'easeOut', delay: 0.15 }}
+        className='absolute -inset-0.5 w-full h-full bg-gradient-to-r from-pink-600 to-violet-600 rounded-2xl blur-lg'
+      />
+      <div className='relative h-full px-10 py-12 mb-0 overflow-hidden bg-black rounded-2xl'>
+        <h3 className='flex items-center justify-center h-12 text-transparent font-secondary bg-clip-text bg-gradient-to-tr via-white from-amber-900 to-amber-900'>
+          ✨ Home is not a house ✨
+        </h3>
+      </div>
+    </motion.div>
+  )
+}
+//  <motion.div initial='initial' whileHover='hover' className='relative'>
+//       <motion.div
+//         variants={glowVariants}
+//         transition={{ ease: 'easeOut', delay: 0.15 }}
+//         className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-violet-600 rounded-xl blur-xl'
+//       />
+//       <motion.div
+//         variants={cardVariants}
+//         transition={{ ease: 'easeOut', delay: 0.15, duration: 0.5 }}
+//         className='relative flex items-center justify-center h-24 bg-black rounded-xl w-96'
+//       >
+//         <h3 className='text-transparent font-secondary bg-clip-text bg-gradient-to-tr via-white from-amber-900 to-amber-900'>
+//           ✨ Home is not a house ✨
+//         </h3>
+//       </motion.div>
+//     </motion.div>
